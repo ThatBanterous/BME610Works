6897ea6930e621cc7a970c09e308a0e967999195
ThatBanterous <dangcamp3@gmail.com>
2025-11-27 11:00:59 +0900

    Fix Rscript installation commands for BiocManager
    Added repository specification and disabled prompts for package installation.

--- week8items/08_2_install_dependency.bash
+++ week8items/08_2_install_dependency.bash
@@ -1,3 +1,5 @@
 #!/bin/bash
 pip3 install --requirement "$(realpath 08_2_requirements.txt)"
-Rscript --vanilla -e 'install.packages("BiocManager")' -e 'BiocManager::install("DNAcopy")'
+
+# FIXED: Added 'repos' to specify the download server (mirror) and 'ask=FALSE' to prevent hanging.
+Rscript --vanilla -e 'install.packages("BiocManager", repos="http://cran.us.r-project.org")' -e 'BiocManager::install("DNAcopy", update=FALSE, ask=FALSE)'
